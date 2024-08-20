7d1c4f9a71eed1dded2475d6ba012293c33d004f
anakassiadsr <anakassiadsr97@gmail.com>
2024-08-20 18:35:12 -0300

    criador simples de planilha

---
+++ app.py
@@ -0,0 +1,27 @@
+import openpyxl
+
+#criar uma planilha
+
+book = openpyxl.Workbook()
+
+#como visualizarpaginas existentes
+print(book.sheetnames)
+
+#como criar uma pagina
+book.create_sheet('Aluno1')
+
+#como selecionar uma pagina
+frutas_page = book['Aluno1']
+frutas_page.append(['Historico','exemplo@gmail.com', 'nome completo'])
+frutas_page.append(['Historico','exemplo@gmail.com', 'nome completo'])
+frutas_page.append(['Historico','exemplo@gmail.com', 'nome completo'])
+frutas_page.append(['Historico','exemplo@gmail.com', 'nome completo'])
+frutas_page.append(['Historico','exemplo@gmail.com', 'nome completo'])
+frutas_page.append(['Historico','exemplo@gmail.com', 'nome completo'])
+frutas_page.append(['Historico','exemplo@gmail.com', 'nome completo'])
+frutas_page.append(['Historico','exemplo@gmail.com', 'nome completo'])
+frutas_page.append(['Historico','exemplo@gmail.com', 'nome completo'])
+frutas_page.append(['Historico','exemplo@gmail.com', 'nome completo'])
+
+#salvara planilha
+book.save('Planilha de Historico - Alunos.xlsx')
